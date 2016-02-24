36cdd4e57cec042d1945f5307a2b24abf0451e3e
jamescapuder <jcapuder@oberlin.edu>
2016-02-24 15:33:18 -0500

    Progress
    Added some to hero; made quest.cs

--- Assets/Resources/Scripts/Hero.cs
+++ Assets/Resources/Scripts/Hero.cs
@@ -1,15 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Hero : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+    //The three hero fields we discussed for this class - the variable names should be relatively self explanitory
+
+    private int numWins;
+    private int experienceLevel;
+    private int heroClass;
+
+    //Optional list of quests for a hero
+    private List<Quest> quests;
+
+
+    // Here we initialize the above variables and require an integer representation of "class" to be
+    //passed as a parameter
+
+	void Start (int Class) {
+        this.numWins = 0;
+        this.experienceLevel = 0;
+        this.heroClass = Class;
+        this.quests = new List<Quest>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+    //General getters/setters for the parameters
+    void addXP(int XP)
+    {
+        this.experienceLevel += XP;
+    }
+
+    int getXP()
+    {
+        return this.experienceLevel;
+    }
+
+    int getWins()
+    {
+        return this.numWins;
+    }
+
+    void addQuest(Quest q)
+    {
+        this.quests.Add(q);
+    }
+
+
 }
